531a00b31ae6f9346d4859121695e827e3695f63
André Assis <andreassis240256@gmail.com>
2026-01-12 03:31:59 -0300

    feat: redesign game flow using State Machine pattern
    Replaced the procedural main loop with a State Machine to manage
    application states.
    
    Implemented States:
    - STATE_START_MENU: Initial ASCII art screen.
    - STATE_GAME: Core gameplay loop.
    - STATE_MENU: Pause/Resume menu.
    - STATE_WINNER/LOSER: End game screens with stats.
    - STATE_EXIT/ERROR: Termination handling.
    
    Also resolved GCC warnings (-Wpedantic, -Wshadow) regarding variable
    declarations and control flow.

--- src/main.c
+++ src/main.c
@@ -16,19 +16,32 @@
 #define KEY_ENTER 13
 #define ESC_KEY 27
 
-enum configurations{
+typedef enum APPstate{
+    STATE_START_MENU,
+    STATE_WINNER,
+    STATE_LOSER,
+    STATE_MENU,
+    STATE_GAME,
+    STATE_EXIT,
+    STATE_ERROR,
+} APPstate;
+
+typedef enum configurations{
     UNLOCKED_CHARS = 1,
     BACKGROUND_COLOR,
     DIFICULTY
-};
+} configurations;
 
-enum comandsProcessInput{
+typedef enum comandsProcessInput{
     TIMEOUT,
     EXIT,
+    GO_TO_MENU,
     GOT_RIGHT,
     GOT_WRONG,
-    DEFEAT
-};
+    DEFEAT,
+    CONTINUE_GAME,
+    PLAY_AGAIN,
+} comandsProcessInput;
 
 const char * dictionary = "/usr/share/dict/words";
 const char * status = "src/configs/status.txt";
@@ -36,10 +49,10 @@ const int max_line_size = 80;
 const int max_configLine_size = 100;
 
 //global variables for theme
-char currentTheme_BG[10];
-char currentStyle_Todo[20];
-char currentStyle_Correct[20];
-char currentStyle_Wrong[20];
+char currentTheme_BG[20];
+char currentStyle_Todo[30];
+char currentStyle_Correct[30];
+char currentStyle_Wrong[30];
 
 int number_words = 20;
 int totalWords;
@@ -64,6 +77,9 @@ void lower(char * str);
 
 //LOADING LEVEL
 //-------------
+char * initFinalStr();
+char * initScreenString(const char * finalStr);
+void printScreenStr(const char * screenStr);
 char ** loadWords();
 
 //auxiliar functions loading level
@@ -80,130 +96,44 @@ int processInputKey(char rKey);
 float getWordsPerMinute(time_t init, time_t end, int strlen);
 int getTypableChars(const char * str, int strSize);
 
+//STATEMACHINE FUNCTIONS
+APPstate runMenu();
+APPstate runGame(float * wpm, float * precision);
+APPstate runStartMenu();
+APPstate runWinnerMenu(float wpm, float precision);
+APPstate runLoserMenu();
+
 int main()
 {
     if(!startFiles())   return 1;
     if(!startConfigs()) return 1;
     srand(time(NULL));
 
-
     enableRawMode();
-    setBeckgroundColor(currentTheme_BG);
-    clearScreen();
-    goHome();
 
-    char ** words = loadWords();
-    if(words == NULL) return 1;
-
-    int len = 0;
-    for(int i = 0; i < number_words; i++)
-        if(words[i] != NULL)
-            len += strlen(words[i]);
-
-    char * finalStr = (char *)malloc((len + number_words * 5 + 100)* sizeof(char));
-    if(finalStr == NULL) return 1;
-    finalStr[0] = '\0';
-
-    for(int i = 0; i < number_words; i++)
-        if(words[i] != NULL)
-            if(i%nColums == nColums - 1){
-                strcat(finalStr, words[i]);
-                strcat(finalStr, "\r\n");
-            }
-            else{
-                strcat(finalStr, words[i]);
-                strcat(finalStr, " ");
-            }
-    
-    int finalLen = strlen(finalStr);
-
-    printf("%s", currentStyle_Todo);
-    for(int i = 0; i < finalLen; i++)
-            if(finalStr[i] == ' ')
-                printf(SYMBOL_SPACE);
-            else if(finalStr[i] == '\r')
-                printf(SYMBOL_ENTER);
-            else if(finalStr[i] == '\n')
-                printf("\r\n");
-            else 
-                printf("%c", finalStr[i]);
-    fflush(stdin);
-    
-    //free words after use
-    for(int i = 0; i < number_words; i++){
-        if(words[i] != NULL) free(words[i]);
-    }
-    free(words);
-    goHome();
-
-    // //MAIN LOGIC
-    // //--------------
-    int nread = 0;
-    int i = 0;
-    int first_letter_typed = 1;
-    int gotWrong = 0;
-    time_t startTime = 0;
-    while(i < finalLen && nread != EXIT && nread != DEFEAT){
-        nread = processInputKey(finalStr[i]);
-
-        if(nread == TIMEOUT) continue;
-        if(nread == EXIT) break;
-
-        if(first_letter_typed){
-            startTime = time(NULL);
-            first_letter_typed = 0;
+    APPstate currentState = runStartMenu();
+    float wpm, precision;
+    while(currentState != STATE_EXIT && currentState != STATE_ERROR)
+        switch (currentState){
+            case STATE_START_MENU:
+                currentState = runStartMenu();
+                break;
+            case STATE_LOSER:
+                currentState = runLoserMenu();
+                break;
+            case STATE_WINNER:
+                currentState = runWinnerMenu(wpm, precision);
+                break;
+            case STATE_MENU:
+                currentState = runMenu();
+                break;
+            case STATE_GAME:
+                currentState = runGame(&wpm, &precision);
+                break;
+            case STATE_EXIT:
+            case STATE_ERROR:
+                break;
         }
-
-        if(nread == GOT_RIGHT){
-            printf("%s", currentStyle_Correct);
-        }
-        else{
-            printf("%s", currentStyle_Wrong);
-            nLives--;
-            gotWrong++;
-        }
-
-        if(finalStr[i] == '\r'){
-            printf(SYMBOL_ENTER "\r\n");
-            i += 2;
-        }
-        else if(finalStr[i] == ' '){
-            printf(SYMBOL_SPACE);
-            i++;
-        }
-        else{
-            printf("%c", finalStr[i]);
-            i++;
-        }
-        
-        printf("%s", currentStyle_Todo);
-        fflush(stdout);
-
-        if(!nLives) nread = DEFEAT;
-            
-    }   
-    time_t finalTime = time(NULL);
-    int typedChar = getTypableChars(finalStr, finalLen);
-
-    if(nread != EXIT){
-        float wordsPerMinute = getWordsPerMinute(startTime, finalTime, finalLen);
-        float precision = 100 * ((float)(typedChar - gotWrong)) / (float)finalLen;
-
-        printf("%s", currentStyle_Correct);
-        if(nLives == 0)
-            printf("\n\n\rYou lost! Good luck next time.\r\n");
-        else{
-            printf("You got it! nice play!\r\n");
-            printf("Great speed, you typed %g words per minute!\r\n", wordsPerMinute);
-            printf("Your precision was %.2f%%!\r\n", precision);
-        }
-        printf("Please press cntr+q or esc to quit\r\n");
-        fflush(stdin);
-
-        nread = 0;
-        while(nread != EXIT)
-            nread = processInputKey('/0');
-    }
 
     //AT EXIT
     //----------------------
@@ -211,7 +141,7 @@ int main()
     printf(COLOR_RESET "\n");
     setBeckgroundColor(COLOR_RESET);
     clearScreen();
-    fflush(stdin);
+    fflush(stdout);
 
     if(dict)    fclose(dict);
     if(stats)  fclose(stats);
@@ -297,7 +227,7 @@ int startConfigs()
                 sprintf(currentStyle_Wrong, "%s%s", currentTheme_BG, COLOR_RED);
                 break;
         
-            case DIFICULTY:
+            case DIFICULTY:{
                 char str[15];
                 for(i = 0; configLine[i+2] != ';' && configLine[i+2] != '\n'; i++)
                     str[i] = tolower(configLine[i+2]);
@@ -316,6 +246,7 @@ int startConfigs()
                     nLives = 7;
                 }
                 break;
+            }
         }
     }
 
@@ -324,6 +255,81 @@ int startConfigs()
 
 
 //LOADING LEVEL
+char * initFinalStr()
+{
+    char ** words = loadWords();
+    if(words == NULL) return NULL;
+
+    int len = 0;
+    for(int i = 0; i < number_words; i++)
+        if(words[i] != NULL)
+            len += strlen(words[i]);
+
+    char * finalStr = (char *)malloc((len + number_words * 5 + 100)* sizeof(char));
+    if(finalStr == NULL) return NULL;
+    finalStr[0] = '\0';
+
+    for(int i = 0; i < number_words; i++){
+        if(words[i] != NULL){
+            if(i%nColums == nColums - 1){
+                strcat(finalStr, words[i]);
+                strcat(finalStr, "\r\n");
+            }
+            else{
+                strcat(finalStr, words[i]);
+                strcat(finalStr, " ");
+            }
+        }
+    }
+    
+    //free words
+    for(int i = 0; i < number_words; i++)
+        if(words[i] != NULL)
+            free(words[i]);
+    free(words);
+
+    return finalStr;
+}
+
+char * initScreenString(const char * finalStr)
+{
+    int count = 0;
+    int finalLen = strlen(finalStr);
+    for(int i = 0; i < finalLen; i++, count++){
+        if(finalStr[i] == '\r')
+            count++;
+    }
+
+    char * screenString = (char *)malloc(sizeof(char) * (count + 1));
+
+    int currentIdx = 0;
+    for(int i = 0; i < finalLen; i++)
+        if(finalStr[i] == '\r')
+            screenString[currentIdx++] = '+';
+        else if(finalStr[i] == '\n'){
+            screenString[currentIdx++] = '\r';
+            screenString[currentIdx++] = '\n';
+        }
+        else{
+            screenString[currentIdx++] = finalStr[i];
+        }
+    screenString[currentIdx] = '\0';
+
+    return screenString;
+}
+
+void printScreenStr(const char * screenStr)
+{
+    int len = strlen(screenStr);
+    for(int i = 0; i < len; i++)
+        if(screenStr[i] == ' ')
+            printf(SYMBOL_SPACE);
+        else if(screenStr[i] == '+')
+            printf(SYMBOL_ENTER);
+        else
+            printf("%c", screenStr[i]);
+}
+
 int isValidWord(const char * str)
 {
     size_t len = strlen(str);
@@ -416,15 +422,18 @@ int processInputKey(char rKey){
 
     if(nread <= 0)  return TIMEOUT;
 
-    if(c == CTRL_KEY('q') || c == CTRL_KEY('c') || c == ESC_KEY){
+    if(c == CTRL_KEY('q') || c == CTRL_KEY('c')){
         return EXIT;
     }
+    if(c == ESC_KEY)
+        return GO_TO_MENU;
 
-    if(rKey == '\r')
+    if(rKey == '\r'){
         if(c == KEY_ENTER || c == '\n')
             return GOT_RIGHT;
         else
             return GOT_WRONG;
+    }
 
     if(c == rKey) 
         return GOT_RIGHT;
@@ -450,9 +459,284 @@ float getWordsPerMinute(time_t init, time_t end, int charCount)
 int getTypableChars(const char * str, int strSize)
 {
     int count = 0;
-    for(int i = 0; str[i] != '/0' && i < strSize; i++){
+    for(int i = 0; str[i] != '\0' && i < strSize; i++){
         if(str[i] != '\r')
             count++;
     }
     return count;
+}
+
+
+//STATEMACHINE FUNCTIONS
+APPstate runMenu() {
+    clearScreen();
+    setBeckgroundColor(currentTheme_BG);
+    goHome();
+    printf("%s", currentStyle_Correct);
+    goHome();
+
+    printf("################  MENU ###############\r\n\r\n");
+    printf("Type 'c' to continue playing\r\n");
+    printf("Type 'cntrl + q' to quit game\r\n");
+    printf("\r\n######################################\r\n");
+    fflush(stdout);
+
+    int nread = 0;
+    char c;
+    int action = 0;
+    while(action != EXIT && action != CONTINUE_GAME){
+        nread = read(STDIN_FILENO, &c, sizeof(c));
+        if(nread == TIMEOUT) continue;
+        
+        printf("%c\r\n", c);
+        if(c == CTRL_KEY('q') || c == ESC_KEY)
+            action = EXIT;
+        else if(c == 'c')
+            action = CONTINUE_GAME;
+    }
+
+    switch (action)
+    {
+    case EXIT:
+        return STATE_EXIT;
+    case CONTINUE_GAME:
+        return STATE_GAME;
+    default:
+        break;
+    }
+
+    return -1;
+}
+
+APPstate runGame(float * wpm, float * precision)
+{
+    int lives = nLives;
+    char * finalStr = initFinalStr();
+    if(finalStr == NULL) return STATE_ERROR;
+    
+    setBeckgroundColor(currentTheme_BG);
+    clearScreen();
+    goHome();
+    printf("%s", currentStyle_Todo);
+    char * screenStr = initScreenString(finalStr);
+    printScreenStr(screenStr);
+    
+    int nread = 0, i = 0, firstLetterTyped = 1;
+    int gotWrong = 0;
+    int finalLen = strlen(finalStr);
+    time_t init, end;
+    goHome();
+    while(i < finalLen && nread != EXIT && nread != DEFEAT && nread != GO_TO_MENU){
+        nread = processInputKey(finalStr[i]);
+
+        if(nread == TIMEOUT) continue;
+        if(nread == EXIT || nread == GO_TO_MENU) break;
+
+        if(firstLetterTyped){
+            init = time(NULL);
+            firstLetterTyped = 0;
+        }
+
+        if(nread == GOT_RIGHT){
+            printf("%s", currentStyle_Correct);
+        }
+        else{
+            printf("%s", currentStyle_Wrong);
+            lives--;
+            gotWrong++;
+        }
+
+        if(finalStr[i] == '\r'){
+            printf(SYMBOL_ENTER "\r\n");
+            i += 2;
+        }
+        else if(finalStr[i] == ' '){
+            printf(SYMBOL_SPACE);
+            i++;
+        }
+        else{
+            printf("%c", finalStr[i]);
+            i++;
+        }
+
+        printf("%s", currentStyle_Todo);
+        fflush(stdout);
+
+        if(!lives) nread = DEFEAT;
+    }
+
+    end = time(NULL); 
+    switch (nread){
+        case EXIT:
+            return STATE_EXIT;
+        case DEFEAT:
+            return STATE_LOSER;
+        case GO_TO_MENU:
+            return STATE_MENU;
+    }
+
+    int charCount = getTypableChars(finalStr, finalLen);
+    *precision = (float)(charCount - gotWrong)/(float)charCount*100;
+    *wpm = getWordsPerMinute(init, end, charCount);
+    
+    return STATE_WINNER;
+}
+
+APPstate runLoserMenu()
+{
+    setBeckgroundColor(currentTheme_BG);
+    goHome();
+
+    printf("################ YOU LOST ##############\r\n\r\n");
+    printf("You lost! Maybe nest time...\r\n");
+    printf("Type 'y' if you would like to play again, or 'ctrl+ q' if you would like to quit\r\n\t");
+    fflush(stdout);
+
+    int nread = 0;
+    char c;
+    while(nread != EXIT && nread != PLAY_AGAIN && nread != GO_TO_MENU){
+        nread = read(STDIN_FILENO, &c, sizeof(c));
+
+        if(nread == TIMEOUT) continue;
+
+        c = tolower(c);
+        if(c == 'y')
+            nread = PLAY_AGAIN;
+        else if(c == CTRL_KEY('q') || c == CTRL_KEY('c'))
+            nread = EXIT;
+        else if(c == ESC_KEY)
+            nread = GO_TO_MENU;
+        else 
+            nread = 0;
+    }
+
+    printf("\r\n");
+    clearScreen();
+    goHome();
+    fflush(stdout);
+
+    switch (nread)
+    {
+    case EXIT:
+        return STATE_EXIT;
+        break;
+    case PLAY_AGAIN:
+        return STATE_GAME;
+    case GO_TO_MENU:
+        return STATE_MENU;
+    default:
+        break;
+    }
+
+    return -1;
+}
+
+APPstate runWinnerMenu(float wpm, float precision)
+{
+    printf("%s", currentStyle_Correct);
+    printf("\r\n\r\n");
+    printf("################# WINNER #####################\r\n");
+    printf("Congratulation! You typed %.2f words per minute, that's really fast!\r\n", wpm);
+    printf("All of that with %.2f%% precision!\n\r", precision);
+    printf("Type 'c' to play again or 'ctrl + q' to quit\r\n");
+    printf("##############################################\r\n");
+    fflush(stdout);
+
+    int action = 0;
+    int nread = 0;
+    char c;
+    while(action != EXIT && action != CONTINUE_GAME && action != GO_TO_MENU){
+        nread = read(STDIN_FILENO, &c, sizeof(c));
+
+        if(nread == TIMEOUT) continue;
+
+        c = tolower(c);
+        printf("%c\r\n", c);
+        if(c == CTRL_KEY('q') || c == CTRL_KEY('c'))
+            action = EXIT;
+        if(c == ESC_KEY)
+            action = GO_TO_MENU;
+        if(c == 'c')
+            action = CONTINUE_GAME;
+    }
+
+    switch (action)
+    {
+    case EXIT:
+        return STATE_EXIT;
+    case GO_TO_MENU:
+        return STATE_MENU;
+    case CONTINUE_GAME:
+        return STATE_GAME;
+    default:
+        break;
+    }
+
+    return -1;
+}
+
+APPstate runStartMenu()
+{
+    const char * initialMsgPath = "src/configs/initialMsg.txt";
+    FILE * initialMsg = fopen(initialMsgPath, "r");
+
+    setBeckgroundColor(currentTheme_BG);
+    clearScreen();
+    goHome();
+    printf("%s", currentStyle_Correct);
+    fflush(stdout);
+    goHome();
+   
+    if(initialMsg == NULL)
+        printf("Welcome to FastTyping! (Art not found)\r\n");
+    else{
+        char line[512];
+        while(fgets(line, sizeof(line), initialMsg))
+        {
+            line[strcspn(line, "\n")] = '\0';
+            printf("%s\r\n", line);
+        }
+
+        fclose(initialMsg);
+    }
+
+    
+    
+    printf("\r\n\r\nHere you can train to type really fast!\r\n");
+    printf("Type ['s'] to start a match.\r\n");
+    printf("Type ['cntrl' + 'q'] to quit aplication.\r\n");
+    fflush(stdout);
+    
+    char command;
+    int nread = 0;
+    int validCommand = 0;
+    while(!validCommand){
+        nread = read(STDIN_FILENO, &command, sizeof(command));
+
+        if(nread == TIMEOUT) continue;
+
+        if(iscntrl(command))
+            printf("'%d'\r\n", command);
+        else
+            printf("%c\r\n", command);
+            
+        command = tolower(command);
+        if(command == 's'){
+            validCommand = 1;
+            return STATE_GAME;
+        }
+        else if(command == CTRL_KEY('q')){
+            validCommand = 1;
+            return STATE_EXIT;
+        }
+        else if(command == ESC_KEY){
+            validCommand = 1;
+            return STATE_MENU;
+        }
+
+        if(!validCommand)
+            printf("Not a valid command, please type it again:\r\n");
+    }
+
+    return -1;
 }
